a4717e31f0977ea6bebbfa1b0e8235e23c4c97ee
Maria Luiza <malualper@hotmail.com>
2020-05-24 14:51:51 -0300

    medidacorreta

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace medidacorreta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           {
+        string metro;
+        double metros;
+        double cm;
+        double km;
+
+        Console.Write("Digite o Valor em Metros ");
+        metro = Console.ReadLine();
+        metros = double.Parse(metro);
+        cm = metros * 100;
+        km = metros / 1000;
+        Console.WriteLine("o valor de cm é " + cm + "cm");
+        Console.WriteLine("o valor de km é " + km + "km");
+
+            
+    }
+}
+}
+    }
+
